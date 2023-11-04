630587b0d66e52e24e49e0dadf07102801e6ca7e
SwornaCode <kasfiamostafa03@gmail.com>
2023-11-04 20:05:34 +0600

    added login and register page

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,8 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../Pages/HomePage/Navbar";
+import Footer from "../Pages/HomePage/Footer";
 
 const MainLayout = () => {
   return (
     <div>
-      
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
--- src/Routers/Router.jsx
+++ src/Routers/Router.jsx
@@ -1,9 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layouts/MainLayout";
+import Home from "../Pages/Home/Home";
+import Login from "../Components/Login/Login"
+import Register from "../Components/Registration/Registration";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <MainLayout></MainLayout>,
+    children:[
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: 'login',
+        element: <Login></Login>
+      },
+      {
+        path: 'register',
+        element: <Register></Register>
+      },
+    ]
   },
 ]);
 
